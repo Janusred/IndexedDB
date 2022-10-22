036b8eb8eb616bc24269bf4dd010353faaecb9d5
Janusred <horaciok300@hotmail.com>
2022-10-22 04:01:08 -0500

    creacion de db

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 
 // indexedDB: Reforzamiento
-
-
+let request = window.indexedDB.open('mi-database', 2);
+request.onupgradeneeded = event => {
+    console.log('Actualización de BD');
+    let db = event.target.result;
+    db.createObjectStore('heroes', {
+        keyPath: 'id'
+    });
+};
 
 
